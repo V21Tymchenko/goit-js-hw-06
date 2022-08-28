a77302a663106ea46aaf8f1ffe5f057345fa081d
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-08-28 18:40:13 +0300

    add task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ulEl = document.querySelector('#ingredients');
+
+const createElement = ingredients.map(ingredient => {
+  const list = document.createElement('li');
+  list.classList.add('item');
+  list.textContent = ingredient;
+  return list;
+});
+
+ulEl.append(...createElement);
+
+// const createLi = elements => {
+//   return elements.map(element => {
+//     const list = document.createElement('li');
+//     list.classList.add('item');
+//     list.textContent = element;
+//     return list;
+//   });
+// };
+// ulEl.append(...createLi);
