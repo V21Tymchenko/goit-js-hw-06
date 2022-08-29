1a2668365c927fbc31c8de60874f5438fe8d0f3b
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-08-29 23:17:38 +0300

    add task-5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const refs = {
+  inputText: document.querySelector('#name-input'),
+  titleText: document.querySelector('#name-output'),
+};
+
+refs.inputText.addEventListener('input', handleButtonEvent);
+
+function handleButtonEvent(event) {
+  //   let inputEL = event.currentTarget.value;
+  //   inputEL !== ' ' ? (refs.titleText.innerText = inputEL.trim()) : 'Anonymous';
+  if (refs.inputText.value !== ' ') {
+    refs.titleText.innerText = refs.inputText.value;
+  } else refs.titleText.innerText = 'Anonymous';
+}
