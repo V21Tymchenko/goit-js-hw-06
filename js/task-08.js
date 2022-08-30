ce36474e33e099482dfe6fa78b0ebb38c2c06dc9
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-08-30 15:14:58 +0300

    add task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Пожалуйста, заполните все поля :)');
+  }
+  const saveData = { email: email.value, password: password.value };
+
+  console.log(saveData);
+  event.currentTarget.reset();
+}
