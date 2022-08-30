ea2b8bbc6da53586dc5e5490ffb7cb8dc756c0f2
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-08-30 18:10:38 +0300

    add task-10 and readme file

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonCr = document.querySelector('[data-create]');
+const buttonDes = document.querySelector('[data-destroy]');
+const box = document.getElementById('boxes');
+
+buttonCr.addEventListener('click', getAmount);
+buttonDes.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+  const amount = document.querySelector('#controls input').value;
+  createBoxes(amount);
+}
+function createBoxes(amount) {
+  const dataSize = 30;
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let size = dataSize + i * 10;
+    let div = document.createElement('div');
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+function destroyBoxes() {
+  box.innerHTML = '';
+}
