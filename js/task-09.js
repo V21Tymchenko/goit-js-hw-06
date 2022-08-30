615250fb44dd7a63c69cd657fdab6c0f6a39771b
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-08-30 16:17:09 +0300

    add task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+button.addEventListener('click', changeBodyColorClick);
+
+function changeBodyColorClick(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
