0136e33995e2bc20fcbaf8d04faf01b2f565eb65
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-09-06 16:01:26 +0300

    upgrade task 6

--- js/task-06.js
+++ js/task-06.js
@@ -5,10 +5,12 @@ input.addEventListener('blur', onTargetInputCkick);
 
 function onTargetInputCkick(event) {
   if (event.currentTarget.value.length === inputLength) {
-    input.classList.add('valid');
-    input.classList.remove('invalid');
+    changesClassInput('valid', 'invalid');
   } else {
-    input.classList.add('invalid');
-    input.classList.remove('valid');
+    changesClassInput('invalid', 'valid');
   }
+}
+function changesClassInput(addCl, rmvCl) {
+  input.classList.add(addCl);
+  input.classList.remove(rmvCl);
 }
